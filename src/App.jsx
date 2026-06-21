72926dbc71b63ff018dceb3bf1772a3a510506ec
Prabhat <Prabhat78961@gmail.com>
2026-06-21 22:51:25 +0530

    Nav Bar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import Highlights from './Components/Highlight'
 
 const App = () => {
   return (
     <main className='bg-black'>
-        Navbar
-        Hero 
-        Highlights
+        <Navbar />
+        <Hero /> 
+        <Highlights />
     </main>
   )
 }
